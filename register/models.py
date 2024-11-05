cb0b30e37e6c94477401bc9a6436e28c41ed4e1e
Muhammadqodir <kod192837465mm@gmail.com>
2024-11-05 14:18:58 +0500

    CustomUser modelini yaratish: foydalanuvchi ismi, avatar va elektron pochta tasdiqlanganligi maydonlari qo'shildi

--- register/models.py
+++ register/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=100, validators=[MinLengthValidator(5)], verbose_name=_('Name'))
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True, verbose_name=_('Avatar'))
+    email_verified = models.BooleanField(default=False, verbose_name=_('Email Verified'))
+
+    def __str__(self):
+        return self.username
+
+
