d51e974f3ca0cf1745434b65372f18f42d8dc1c3
Muhammadqodir <kod192837465mm@gmail.com>
2024-11-05 14:23:02 +0500

    Foydalanuvchini ro'yxatdan o'tkazish funksiyasi qo'shildi: foydalanuvchi formani to'ldirib tizimga kirishi uchun

--- register/views.py
+++ register/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect('home')  
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'register/register.html', {'form': form})
+
