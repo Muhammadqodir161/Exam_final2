b4ab3451f261841fd63dae415d9785083b6fa285
Muhammadqodir <kod192837465mm@gmail.com>
2024-11-05 13:34:42 +0500

    Foydalanuvchining izdoshlari postlarini ko'rsatish uchun HomePageView yaratish
    - HomePageView klassi yaratildi, u ListViewdan meros oladi va foydalanuvchining izdoshlari tomonidan yozilgan postlarni ko'rsatadi.
    - get_queryset metodida, faqat foydalanuvchining profiliga izdoshlari bo'lgan postlar tanlab olindi.
    - Postlar created_at maydoni bo'yicha kamayish tartibida tartiblangan.
    - LoginRequiredMixin qo'shildi, shuning uchun faqat tizimga kirgan foydalanuvchilar ushbu sahifani ko'rishlari mumkin.

--- blog_site/views.py
+++ blog_site/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
+from .models import Profile, Post, Comment, Subscription, Notification
+from .forms import PostForm, CommentForm, ProfileForm
 
-# Create your views here.
+
+class HomePageView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'index.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        return Post.objects.filter(author__profile__followers=self.request.user.profile).order_by('-created_at')
